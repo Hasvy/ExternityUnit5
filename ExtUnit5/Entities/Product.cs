2dfb38c2d845994c9cfa2674dbf081cf0379bc8e
Roman Zavalishin <roman.zavalishin@webcom.cz>
2024-06-29 18:22:10 +0200

    Completed

--- ExtUnit5/Entities/Product.cs
+++ ExtUnit5/Entities/Product.cs
@@ -1,15 +1,31 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ExtUnit5.Entities
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Název je povinný")]
         public string Name { get; set; } = null!;
+
         [AllowNull]
+        [MaxLength(255)]
+        [StringLength(255, ErrorMessage = "Description nesmí být delší než 255 znaků.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Cena je povinná")]
         public float Price { get; set; }
+
         [AllowNull]
+        [Range(0, int.MaxValue, ErrorMessage = "Počet skladem musí být nezáporné číslo")]
         public int Stock { get; set; } = 0;
+
+        [NotMapped]
+        public bool IsActive => Stock > 0;
     }
 }
